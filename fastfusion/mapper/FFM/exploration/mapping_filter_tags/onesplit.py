28a14d891052944fabdef8804a64fe6d347a6fbd
Michael Gilbert <gilbertm@mit.edu>
2025-05-31 17:15:38 -0400

    [filter] Refactor LoopTree and TileFlow to use onesplit; TileFlow should be further run with even only

--- fastfusion/mapper/FFM/exploration/mapping_filter_tags/onesplit.py
+++ fastfusion/mapper/FFM/exploration/mapping_filter_tags/onesplit.py
@@ -1,7 +1,7 @@
 from fastfusion.frontend.mapping import Storage, Iteration
 
 
-def get_looptree_tag_mha(pmapping, intermediate_tensors, non_fused_memory):
+def get_one_split_tag(pmapping, intermediate_tensors, non_fused_memory):
     tensor_to_n_fused_loops = {t: None for t in intermediate_tensors}
     n_loops = 0
     for node in pmapping:
@@ -21,16 +21,16 @@ def get_looptree_tag_mha(pmapping, intermediate_tensors, non_fused_memory):
 
     # Unfused
     if all(t is None for t in tensor_to_n_fused_loops.values()):
-        return ("LOOPTREE_VALID",)
+        return ("ONE_SPLIT",)
 
     # Fused with one side but not the other. We don't want to interfere with the
-    # unfused side, so just go LOOPTREE_VALID. The number of loops will be enforced
+    # unfused side, so just go ONE_SPLIT. The number of loops will be enforced
     # by the tiling since it must match for the one fused tensor.
     if len(tensor_to_n_fused_loops) == 1:
-        return ("LOOPTREE_VALID",)
+        return ("ONE_SPLIT",)
     
     # Fused with both sides. Make sure that the number of loops is the same.
     unique_loops = set(t for t in tensor_to_n_fused_loops.values() if t is not None)
     if len(unique_loops) > 1:
-        return ("LOOPTREE_INVALID",)
-    return ("LOOPTREE_VALID", f"FUSED_LOOPS={next(iter(unique_loops))}")
+        return ("NOT_ONE_SPLIT",)
+    return ("ONE_SPLIT", f"FUSED_LOOPS={next(iter(unique_loops))}")
