8420055bc2edc6012d89c8645f67472d68dc30b0
Michael Gilbert <gilbertm@mit.edu>
2025-06-19 17:57:12 -0400

    [util] sympy Max bugfix

--- fastfusion/util/sympy/broadcast_max.py
+++ fastfusion/util/sympy/broadcast_max.py
@@ -5,8 +5,5 @@ def Max(a, *bs):
     """More post-lambdify broadcast-friendly option than sympy.Max"""
     result = a
     for b in bs:
-        if isinstance(result, sympy.Expr) and isinstance(b, sympy.Expr):
-            result = sympy.Max(result, b)
-        else:
-            result = sympy.Piecewise((result, result > b), (b, True))
+        result = sympy.Piecewise((result, result > b), (b, True))
     return result
