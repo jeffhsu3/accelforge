d1f2d83eec366bf67835dafde492900fdf5d25b9
tanner-andrulis <andrulis@mit.edu>
2025-07-22 18:15:04 +0000

    Remove debug code

--- fastfusion/mapper/FFM/joining/mappinginfo.py
+++ fastfusion/mapper/FFM/joining/mappinginfo.py
@@ -164,19 +164,16 @@ class Compatibility(Updatable):
     splits: fzs[Split] = fzs()
     tags: Tags = Tags(fzs())
     
-    def __hash__(self):
+    def loops(self) -> tuple[Loop, ...]:
         if self.tensors:
-            loop_bounds = tuple(l.bound for l in next(iter(self.tensors)).loops)
-        else:
-            loop_bounds = tuple()
-        return hash((self.n_loops, loop_bounds, self.tensors, self.tags))
+            return next(iter(self.tensors)).loops
+        return tuple()
+    
+    def __hash__(self):
+        return hash((self.n_loops, self.tensors, self.tags))
     
     def __eq__(self, other):
-        if self.tensors:
-            loop_bounds = tuple(l.bound for l in next(iter(self.tensors)).loops)
-        else:
-            loop_bounds = tuple()
-        return (self.n_loops, loop_bounds, self.tensors, self.tags) == (other.n_loops, loop_bounds, other.tensors, other.tags)
+        return (self.n_loops, self.tensors, self.tags) == (other.n_loops, other.tensors, other.tags)
 
     def __post_init__(self):
         assert isinstance(self.n_loops, int)
