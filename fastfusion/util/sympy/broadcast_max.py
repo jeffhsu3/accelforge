d1de289192d1961652317e5dec583ae43b50b5f6
tanner-andrulis <andrulis@mit.edu>
2025-08-13 20:24:49 +0000

    Broadcast max bugfix

--- fastfusion/util/sympy/broadcast_max.py
+++ fastfusion/util/sympy/broadcast_max.py
@@ -1,25 +1,18 @@
 import sympy
+from sympy import Max
+from sympy import Min
 
+# MAX BUG FIX.
+# def Min(a, *bs):
+#     """More post-lambdify broadcast-friendly option than sympy.Min"""
+#     result = a
+#     for b in bs:
+#         result = sympy.Piecewise((result, result < b), (b, True))
+#     return result
 
-def Min(a, *bs):
-    """More post-lambdify broadcast-friendly option than sympy.Min"""
-    result = a
-    for b in bs:
-        result = sympy.Piecewise((result, result < b), (b, True))
-    return result
-
-
-
-def Max(a, *bs):
-    """More post-lambdify broadcast-friendly option than sympy.Max"""
-    result = a
-    for b in bs:
-        result = sympy.Piecewise((result, result > b), (b, True))
-    return result
-
-def Min(a, *bs):
-    """More post-lambdify broadcast-friendly option than sympy.Min"""
-    result = a
-    for b in bs:
-        result = sympy.Piecewise((result, result < b), (b, True))
-    return result
+# def Max(a, *bs):
+#     """More post-lambdify broadcast-friendly option than sympy.Max"""
+#     result = a
+#     for b in bs:
+#         result = sympy.Piecewise((result, result > b), (b, True))
+#     return result
